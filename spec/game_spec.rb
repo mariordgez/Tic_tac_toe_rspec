a09a5f9ba8a0eded4f4f862e814719d7105e972e
Mario Rodriguez <mariordgez@hotmail.com>
2021-05-14 17:40:50 -0500

    Added move rspec test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,10 +14,18 @@ describe Game do
 end
 end
 
-  describe "#input_to index" do
+  describe "#input_to_index" do
   it "Should return the input number minus 1" do
-    expect(games.input_to_index(5)).to eql(4)
+    expect(games.input_to_index('5')).to eql(4)
 end
 end
+
+describe '#move' do
+it "Should change the board slected number with the player sign" do 
+  games.move(0,"X")
+  expect(games.board[0]).to eql('X')
+end
+end
+
 
 end
