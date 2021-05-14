64a5a69d334307b7a4393c7a0ab633d1af0fa159
Mario Rodriguez <mariordgez@hotmail.com>
2021-05-14 17:22:29 -0500

    Added display_board and input to index methods in the rspec test

---
+++ spec/game_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../lib/game'
+require_relative '../lib/player'
+
+describe Game do
+  let(:games){Game.new}
+  describe "#display_board" do
+  it "Should display a board with the numbers from 1-9" do
+  board=  " 1 | 2 | 3 \n" \
+          "------------\n" \
+          " 4 | 5 | 6 \n" \
+          "------------\n" \
+          " 7 | 8 | 9 \n"
+  expect(games.display_board).to eql(board)
+end
+end
+
+  describe "#input_to index" do
+  it "Should return the input number minus 1" do
+    expect(games.input_to_index(5)).to eql(4)
+end
+end
+
+end
